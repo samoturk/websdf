3ebc670688ae064748856536f6c3046069f3792a
Samo Turk <samo.turk@gmail.com>
2015-02-02 00:14:25 +0100

    Option to calculate descriptors

--- websdf/calculations.py
+++ websdf/calculations.py
@@ -1,8 +1,52 @@
 from rdkit import Chem
 from rdkit.Chem import Draw
+from rdkit.Chem import Descriptors
 from rdkit.Chem import PandasTools
 
-def read_sdf(sdf):
+
+def _clogSw(mol):
+    '''
+    Inspired by work by Christos Kannas presented at RDKit UGM 2013
+    Based on:
+    J. S. Delaney, Journal of Chemical Information and Modeling, 44, 1000-1005, 2004.
+    '''
+    MolWeight = Descriptors.MolWt(mol)
+    clogP = Descriptors.MolLogP(mol)
+    RotBonds = Descriptors.NumRotatableBonds(mol)
+    aromaticHeavyatoms = len(mol.GetSubstructMatches(Chem.MolFromSmarts("[a]")))
+    numAtoms = mol.GetNumAtoms()
+    AromProp = float(aromaticHeavyatoms) / numAtoms
+    # New clogSw with coefficients from Christos' presentation
+    clogSw_value = 0.233743817233 \
+                    -0.74253027 * clogP \
+                    -0.00676305 * MolWeight \
+                    +0.01580559 * RotBonds \
+                    -0.35483585 * AromProp
+    return clogSw_value
+
+def read_sdf(sdf, checks):
     df = PandasTools.LoadSDF(sdf)
+    
+    # For compatibility with RDKit older than 2015_03
+    if 'SMILES' in list(df.columns):
+        df = df.drop(['SMILES'], axis=1)
+    
+    # Recalculate SMILES
+    if 'SMILES' in checks:
+        df['SMILES'] = df.apply(lambda x: Chem.MolToSmiles(x['ROMol']), axis=1)
+    # Calculate MW
+    if 'MW' in checks:
+        df['MW'] = df['ROMol'].map(Descriptors.MolWt).round(decimals=2)
+    # Calculate logP
+    if 'logP' in checks:
+        df['logP'] = df['ROMol'].map(Descriptors.MolLogP).round(decimals=2)
+    # Calculate H-bond donors and acceptors    
+    if 'HB' in checks:
+        df['HBA'] = df['ROMol'].map(Descriptors.NumHAcceptors)
+        df['HBD'] = df['ROMol'].map(Descriptors.NumHDonors)
+    # Calculate solubility    
+    if 'logS' in checks:
+        df['clogSw'] = df['ROMol'].map(_clogSw).round(decimals=2)
+        
     return df
     
--- websdf/views.py
+++ websdf/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render, render_to_response, redirect
 from .forms import UploadFileForm
 from .calculations import read_sdf
 
@@ -8,8 +8,10 @@ def home(request):
     '''
     Define home page view. It is loaded by urls.py.
     '''
-    
-    return render(request, 'index.html', {'form':UploadFileForm})
+    if 'error' in request.GET:
+        return render(request, 'index.html', {'form':UploadFileForm, 'error':request.GET['error']})
+    else:
+        return render(request, 'index.html', {'form':UploadFileForm})
 
 def upload_file(request):
     '''
@@ -17,9 +19,10 @@ def upload_file(request):
     '''
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
-        if form.is_valid():
-            df = read_sdf(request.FILES['file'])
-            
+        if form.is_valid() and str(request.FILES['file']).endswith('.sdf'):
+            checks = request.POST.getlist('checks')
+            df = read_sdf(request.FILES['file'], checks)
+                
             # Get table columns
             cols = list(df.columns)
             
@@ -27,7 +30,14 @@ def upload_file(request):
             rows = []
             for ix, row in df.iterrows():
                 rows.append(list(row))
-            print rows
+            print checks
+            #print rows
             return render(request, 'table.html', {'rows':rows, 'cols':cols})
+        else:
+            error = 'Please select a valid SDF file'
+        
+    else:
+        error = 'Please select a valid SDF file'
+    return redirect('/?error=%s' %error)
         
 
