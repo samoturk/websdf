2fd721d3e9dd8d2074e7c75e3f1270b7d7ee1186
Samo Turk <samo.turk@gmail.com>
2015-10-18 18:59:30 +0200

    SMILES support

--- websdf/calculations.py
+++ websdf/calculations.py
@@ -1,14 +1,15 @@
 from rdkit import Chem
-from rdkit.Chem import Draw
 from rdkit.Chem import Descriptors
 from rdkit.Chem import PandasTools
+import pandas as pd
 
 
 def _clogSw(mol):
     '''
     Inspired by work by Christos Kannas presented at RDKit UGM 2013
     Based on:
-    J. S. Delaney, Journal of Chemical Information and Modeling, 44, 1000-1005, 2004.
+    J. S. Delaney, Journal of Chemical Information and Modeling, 44, 1000-1005,
+    2004.
     '''
     MolWeight = Descriptors.MolWt(mol)
     clogP = Descriptors.MolLogP(mol)
@@ -24,17 +25,14 @@ def _clogSw(mol):
                     -0.35483585 * AromProp
     return clogSw_value
 
-def read_sdf(sdf, checks):
-    df = PandasTools.LoadSDF(sdf)
+def _calculate_descs(df, checks):
     # Create a # column
     df['#'] = range(len(df))
     # Put # column on first position
-    df = df[['#'] + [x for x in list(df.columns) if x != '#']]
-    
+    df = df[['#'] + [x for x in list(df.columns) if x != '#']]    
     # For compatibility with RDKit older than 2015_03
     if 'SMILES' in list(df.columns):
         df = df.drop(['SMILES'], axis=1)
-    
     # Recalculate SMILES
     if 'SMILES' in checks:
         df['SMILES'] = df.apply(lambda x: Chem.MolToSmiles(x['ROMol']), axis=1)
@@ -51,6 +49,16 @@ def read_sdf(sdf, checks):
     # Calculate solubility    
     if 'logS' in checks:
         df['clogSw'] = df['ROMol'].map(_clogSw).round(decimals=2)
-        
+    return df
+
+def read_sdf(sdf, checks):
+    df = PandasTools.LoadSDF(sdf)
+    df = _calculate_descs(df,checks)
+    return df
+
+def read_smi(smi, checks):
+    df = pd.read_csv(smi, delimiter="\t", names=['SMILES', 'ID'])
+    PandasTools.AddMoleculeColumnToFrame(df, smilesCol='SMILES')
+    df = _calculate_descs(df,checks)
     return df
     
--- websdf/views.py
+++ websdf/views.py
@@ -1,7 +1,5 @@
-from django.shortcuts import render, render_to_response, redirect
-from .calculations import read_sdf
-
-import pandas as pd
+from django.shortcuts import render, redirect
+from .calculations import read_sdf, read_smi
 
 def home(request):
     '''
@@ -28,11 +26,27 @@ def upload_file(request):
             rows = []
             for ix, row in df.iterrows():
                 rows.append(zip(cols,list(row)))
-            print checks
+            #print checks
             #print rows
             return render(request, 'table.html', {'rows':rows, 'cols':cols})
+        elif (str(request.FILES['file']).endswith('.smi') or str(request.FILES[
+            'file']).endswith('.ism')):
+            checks = request.POST.getlist('checks')            
+            df = read_smi(request.FILES['file'], checks)
+                
+            # Get table columns
+            cols = list(df.columns)
+            
+            # Extract DataFrame rows
+            rows = []
+            for ix, row in df.iterrows():
+                rows.append(zip(cols,list(row)))
+            #print checks
+            #print rows
+            return render(request, 'table.html', {'rows':rows, 'cols':cols})
+            
         else:
-            error = 'Please select a valid SDF file'
+            error = 'Please select a valid SDF/SMI file'
         
     else:
         error = 'Please select a valid SDF file'
